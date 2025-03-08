81741ba64c3452eebc8d594e2ec3b4415bfec02d
Kim GwangJin <kkj9491@knou.ac.kr>
2025-03-08 13:23:06 +0900

    기본레이아웃 수정

--- src/app/kanban/page.tsx
+++ src/app/kanban/page.tsx
@@ -1,14 +1,12 @@
-// 'use client'
+'use client'
 import React from "react";
-// import KanbanBoard from "@/components/kanban/KanbanBoard";
+import KanbanBoard from "@/components/kanban/KanbanBoard";
 
-export default function Home() {
-
-
+export default function BoardPage() {
 
     return (
         <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-            <div>DashBoard</div>
+            <div><KanbanBoard /></div>
         </main>
     );
 }
--- src/components/common/NavItem.tsx
+++ src/components/common/NavItem.tsx
@@ -16,11 +16,16 @@ const NavItem = ({ mobile, currentUser } : NavItemProps)=> {
     return (
         <ul className={`text-md justify-center flex gap-4 w-full items-center ${mobile && "flex-col h-full"}`}>
             {/*<li className="py-2 text-center border-b-4 cursor-pointer"><Link href="/admin">Admin</Link></li>*/}
-            <li className="py-2 text-center border-b-4 cursor-pointer"><Link href="/user">User</Link></li>
+            {/*<li className="py-2 text-center border-b-4 cursor-pointer"><Link href="/user">User</Link></li>*/}
             {currentUser ? (
-                <li className="py-2 text-center border-b-4 cursor-pointer">
-                    <button onClick={() => signOut()}>Sign Out</button>
-                </li>
+                <>
+                    <li className="py-2 text-center border-b-4 cursor-pointer">
+                        <Link href="/kanban">Kanban Board</Link>
+                    </li>
+                    <li className="py-2 text-center border-b-4 cursor-pointer">
+                        <button onClick={() => signOut()}>Sign Out</button>
+                    </li>
+                </>
             ) : (
                 <>
                     <li className="py-2 text-center border-b-4 cursor-pointer">
